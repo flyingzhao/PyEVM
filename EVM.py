76ff054693cbc3c8691f48e9785a0c1f769fd2f2
flyingzhao <1142749677@qq.com>
2016-09-22 18:16:10 +0800

    ntsc2rbg

--- EVM.py
+++ EVM.py
@@ -1,16 +1,15 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 def rgb2ntsc(src):
-    rows=img.shape[0]
-    cols=img.shape[1]
+    rows=src.shape[0]
+    cols=src.shape[1]
     dst=np.zeros((rows,cols,3),dtype=np.float64)
     for i in range(rows):
         for j in range(cols):
-            B=img[i,j][0]  #B
-            G=img[i,j][1]  #G
-            R=img[i,j][2]  #R
+            B=src[i,j][0]  #B
+            G=src[i,j][1]  #G
+            R=src[i,j][2]  #R
             T=np.array([[0.114,0.587,0.298],[-0.321,-0.275,0.596],[0.311,-0.528,0.212]])
             [Y,I,Q]=np.dot(T,np.array([[B],[G],[R]]))
             dst[i,j][0]=Y
@@ -19,6 +18,21 @@ def rgb2ntsc(src):
     return dst
 
 def ntsc2rbg(src):
+    rows=src.shape[0]
+    cols=src.shape[1]
+    dst=np.zeros((rows,cols,3),dtype=np.float64)
+
+    for i in range(rows):
+        for j in range(cols):
+            Y=src[i,j][0]
+            I=src[i,j][1]
+            Q=src[i,j][2]
+            T=np.array([[1,-1.108,1.705],[1,-0.272,-0.647],[1,0.956,0.620]])
+            [B,G,R]=np.dot(T,np.array([[Y],[I],[Q]]))
+            dst[i,j][0]=B
+            dst[i,j][1]=G
+            dst[i,j][2]=R
+    return dst
 
 
 
@@ -28,7 +42,11 @@ if __name__=="__main__":
     img=cv2.imread("1.jpg")
     cv2.imshow("hello", img)
     print(img[0,0])
-    nst=rgb2ntsc(img[:,:,0])
+    nst=rgb2ntsc(img)
     cv2.imshow("h",nst)
-    print(nst[9,9])
+    print(nst[0,0])
+    bgr=ntsc2rbg(nst)
+    cv2.imshow("hh", bgr)
+    print(bgr[0, 0])
+
     cv2.waitKey()

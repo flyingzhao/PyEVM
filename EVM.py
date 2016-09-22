56c6469a9e4cc59ec3ede695323c6017cfeb255f
flyingzhao <1142749677@qq.com>
2016-09-22 18:30:20 +0800

    vectorized

--- EVM.py
+++ EVM.py
@@ -5,36 +5,21 @@ def rgb2ntsc(src):
     rows=src.shape[0]
     cols=src.shape[1]
     dst=np.zeros((rows,cols,3),dtype=np.float64)
+    T = np.array([[0.114, 0.587, 0.298], [-0.321, -0.275, 0.596], [0.311, -0.528, 0.212]])
     for i in range(rows):
         for j in range(cols):
-            B=src[i,j][0]  #B
-            G=src[i,j][1]  #G
-            R=src[i,j][2]  #R
-            T=np.array([[0.114,0.587,0.298],[-0.321,-0.275,0.596],[0.311,-0.528,0.212]])
-            [Y,I,Q]=np.dot(T,np.array([[B],[G],[R]]))
-            dst[i,j][0]=Y
-            dst[i,j][1]=I
-            dst[i,j][2]=Q
+            dst[i, j]=np.dot(T,src[i,j])
     return dst
 
 def ntsc2rbg(src):
     rows=src.shape[0]
     cols=src.shape[1]
     dst=np.zeros((rows,cols,3),dtype=np.float64)
-
+    T = np.array([[1, -1.108, 1.705], [1, -0.272, -0.647], [1, 0.956, 0.620]])    #todo:T改成矩阵的逆运算
     for i in range(rows):
         for j in range(cols):
-            Y=src[i,j][0]
-            I=src[i,j][1]
-            Q=src[i,j][2]
-            T=np.array([[1,-1.108,1.705],[1,-0.272,-0.647],[1,0.956,0.620]])
-            [B,G,R]=np.dot(T,np.array([[Y],[I],[Q]]))
-            dst[i,j][0]=B
-            dst[i,j][1]=G
-            dst[i,j][2]=R
+            dst[i, j]=np.dot(T,src[i,j])
     return dst
-
-
 
 
 if __name__=="__main__":

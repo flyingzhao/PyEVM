ae1a891e877b192c9c9a408a365e89a8c99db4f3
flyingzhao <1142749677@qq.com>
2016-09-22 19:44:22 +0800

    build gaussian and laplacian pyramid

--- EVM.py
+++ EVM.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+#convert RBG to YIQ
 def rgb2ntsc(src):
     rows=src.shape[0]
     cols=src.shape[1]
@@ -11,27 +12,45 @@ def rgb2ntsc(src):
             dst[i, j]=np.dot(T,src[i,j])
     return dst
 
+#convert YIQ to RBG
 def ntsc2rbg(src):
     rows=src.shape[0]
     cols=src.shape[1]
     dst=np.zeros((rows,cols,3),dtype=np.float64)
-    T = np.array([[1, -1.108, 1.705], [1, -0.272, -0.647], [1, 0.956, 0.620]])    #todo:T改成矩阵的逆运算
+    T = np.array([[1, -1.108, 1.705], [1, -0.272, -0.647], [1, 0.956, 0.620]])    #todo:T改成矩阵的逆
     for i in range(rows):
         for j in range(cols):
             dst[i, j]=np.dot(T,src[i,j])
     return dst
 
+#Build Gaussian Pyramid
+def buildGaussianPyramid(src,level=3):
+    s=src.copy()
+    pyramid=[s]
+    for i in range(level):
+        s=cv2.pyrDown(s)
+        pyramid.append(s)
+    return pyramid
+
+#Build Laplacian Pyramid
+def buildLaplacianPyramid(src,levels=3):
+    gaussianPyramid = buildGaussianPyramid(src, levels)
+    s=src.copy()
+    pyramid=[gaussianPyramid[levels]]
+    for i in range(levels,0,-1):
+        GE=cv2.pyrUp(gaussianPyramid[i])
+        L=cv2.subtract(gaussianPyramid[i-1],GE)
+        pyramid.append(L)
+    return pyramid
 
 if __name__=="__main__":
 
     img=cv2.imread("1.jpg")
     cv2.imshow("hello", img)
-    print(img[0,0])
-    nst=rgb2ntsc(img)
-    cv2.imshow("h",nst)
-    print(nst[0,0])
-    bgr=ntsc2rbg(nst)
-    cv2.imshow("hh", bgr)
-    print(bgr[0, 0])
-
+    # p=buildGaussianPyramid(img)
+    p=buildLaplacianPyramid(img)
+    cv2.imshow("0",p[0])
+    cv2.imshow("1", p[1])
+    cv2.imshow("2", p[2])
+    cv2.imshow("3", p[3])
     cv2.waitKey()

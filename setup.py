1d7b5f5b90a8ccdf96d49ce672c9b4730941c3d5
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-02-13 15:10:50 +0100

    code format corrected

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
-        #'Programming Language :: Python :: Implementation :: PyPy',
+        # 'Programming Language :: Python :: Implementation :: PyPy',
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',

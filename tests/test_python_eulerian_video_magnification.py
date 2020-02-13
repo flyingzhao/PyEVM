d9e052f07c76eab6625c269e699871b35306e68f
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-02-13 14:50:17 +0100

    modified first test

--- tests/test_python_eulerian_video_magnification.py
+++ tests/test_python_eulerian_video_magnification.py
@@ -1,6 +1,11 @@
 
+import pytest
+
 from python_eulerian_video_magnification.cli import main
 
 
-def test_main():
-    main([])
+def test_main_as_smoke_test_wrong_filename():
+    """This tests the setup of main for wrong input file only as a smoke test, as I'm not testing arparse"""
+    with pytest.raises(SystemExit) as e:
+        main(['fu.bar'])
+    assert "2" in str(e.value)

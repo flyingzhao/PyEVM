a15f00c5f116866be8cbf18e9749d53081135d91
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-03-04 12:59:08 +0100

    added timestamp for the processed output file

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,25 +1,30 @@
 import os.path
 
+from datetime import datetime
 from python_eulerian_video_magnification.metadata import MetaData
 from python_eulerian_video_magnification.mode import Mode
 
 
-def test_output_filename():
+def test_output_filename(monkeypatch):
+    monkeypatch.setattr(MetaData, "get_date", lambda: "2020-01-01-16:16:16")
     out = MetaData.output_file_name("abd.avi", "color", "/fu/bar/")
-    assert out == "/fu/bar/abd_color_evm.avi"
+    assert out == "/fu/bar/abd_color_evm_2020-01-01-16:16:16.avi"
 
 
-def test_output_filename_with_path():
+def test_output_filename_with_path(monkeypatch):
+    monkeypatch.setattr(MetaData, "get_date", lambda: "2020-01-01-16:16:16")
     out = MetaData.output_file_name("/tmp/abd.avi", "color", "/fu/bar/")
-    assert out == "/fu/bar/abd_color_evm.avi"
+    assert out == "/fu/bar/abd_color_evm_2020-01-01-16:16:16.avi"
 
 
-def test_output_filename_with_path_for_metadata():
+def test_output_filename_with_path_for_metadata(monkeypatch):
+    monkeypatch.setattr(MetaData, "get_date", lambda: "2020-01-01-16:16:16")
     out = MetaData.output_file_name("/tmp/abd.avi", "color", "/fu/bar/", generate_json=True)
-    assert out == "/fu/bar/abd_color_evm.json"
+    assert out == "/fu/bar/abd_color_evm_2020-01-01-16:16:16.json"
 
 
-def test_for_data_as_expected():
+def test_for_data_as_expected(monkeypatch):
+    monkeypatch.setattr(MetaData, "get_date", lambda: "2020-01-01-16:16:16")
     sut = MetaData(
         file_name="/fu/bar/gob.avi",
         output_folder="/out/put/",
@@ -31,8 +36,9 @@ def test_for_data_as_expected():
         amplification=23
     )
     assert getattr(sut, "_MetaData__data") == {'file': "/fu/bar/gob.avi", 'output': "/out/put/",
-                                               'target': "/out/put/gob_color_evm.avi",
-                                               'meta_target': "/out/put/gob_color_evm.json", 'low': 0.1, 'high': 2.1,
+                                               'target': "/out/put/gob_color_evm_2020-01-01-16:16:16.avi",
+                                               'meta_target': "/out/put/gob_color_evm_2020-01-01-16:16:16.json",
+                                               'low': 0.1, 'high': 2.1,
                                                'levels': 4,
                                                'amplification': 23, 'mode': 'COLOR', 'suffix': 'color', 'date': None}
 

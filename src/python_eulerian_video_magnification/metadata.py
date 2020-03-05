b731e2a36faa5d2f66015dfb202b327bc42476e8
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-03-05 11:06:18 +0100

    changed timeformat to a more windows friendly version

--- src/python_eulerian_video_magnification/metadata.py
+++ src/python_eulerian_video_magnification/metadata.py
@@ -30,17 +30,22 @@ class MetaData:
     def output_file_name(filename: str, suffix: str, path: str, generate_json=False):
         filename_split = os.path.splitext(os.path.split(filename)[-1])
         extension = ".json" if generate_json else filename_split[1]
-        return os.path.join(path, filename_split[0] + "_%s_evm_%s" % (suffix, MetaData.get_date()) + extension)
+        return os.path.join(path, filename_split[0] + "_%s_evm_%s" %
+                            (suffix, MetaData.format_date(MetaData.get_date())) + extension)
 
     def __getitem__(self, item):
         return self.__data[item]
 
     def save_meta_data(self):
         """stores the meta data dictionary as a json"""
-        self.__data['date'] = MetaData.get_date()
+        self.__data['date'] = MetaData.format_date(MetaData.get_date())
         with open(self.__data['meta_target'], 'w') as fp:
             json.dump(self.__data, fp=fp, sort_keys=True, indent=4, separators=(',', ': '))
 
     @staticmethod
-    def get_date() -> str:
-        return str(datetime.now()).replace(' ', '-').split('.')[0]
+    def get_date() -> datetime:
+        return datetime.now()
+
+    @staticmethod
+    def format_date(date: datetime) -> str:
+        return date.strftime("%Y-%m-%d-%H-%M-%S")
--- tests/test_video_motion_handling.py
+++ tests/test_video_motion_handling.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from python_eulerian_video_magnification.cli import main
 from python_eulerian_video_magnification.metadata import MetaData
@@ -7,7 +8,7 @@ sample = os.path.join(os.getcwd(), 'tests', 'videos', 'guitar1sec.mp4')
 
 
 def test_process_video(tmp_path, capsys, monkeypatch):
-    monkeypatch.setattr(MetaData, "get_date", lambda: "2020-01-01-16:16:16")
+    monkeypatch.setattr(MetaData, "get_date", lambda: datetime(year=2020, month=1, day=1, hour=16, minute=16, second=16, microsecond=1))
     main([sample, '-o', str(tmp_path)])
 
     captured = capsys.readouterr()
